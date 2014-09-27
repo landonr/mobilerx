d8d5af486627cfed330b7fc437dc1ce39cd8279a
Jason Wist <jason.wist@gmail.com>
2014-09-27 17:03:32 -0600

    firebase + form

--- pharma_view/js/rx_scripts.js
+++ pharma_view/js/rx_scripts.js
@@ -1,26 +1,52 @@
-var myFirebaseRef = new Firebase("https://dazzling-torch-5422.firebaseio.com/");
+var myFirebaseRef = new Firebase("https://amber-inferno-3172.firebaseio.com/");
 
 
 $(document).ready(function(){
-	$(".test").click(function(){
+	$("#createRx").click(function(){
 		console.log("createRxclicked")
-		createRx();
-	});
-});
 
-function createRx() {
-	console.log("createRxFcn");
-	myFirebaseRef.set({
-		"id": 1,
-		"drug" : [{
-			"name": "designer-drug",
-			"number": 22,
-			"mass": 22,
-			"quantity": 2
-		}],
-		"patientId": 8675309,
-		"status": "pending"
+		var patient_id = $('#patient-id').val();
+ 		var drug_name = $('#drug-name').val();
+ 		var drug_number = $('#drug-number').val();
+ 		var drug_mass = $('#drug-mass').val();
+ 		var drug_quantity = $('#drug-quantity').val();
+
+ 		var rx_ref = myFirebaseRef.child('rx');
+
+ 		rx_ref.push({
+ 			patientId: patient_id,
+ 			drug: [{
+ 				name: drug_name,
+ 				number: drug_number,
+ 				mass: drug_mass,
+ 				quantity: drug_quantity
+ 			}],
+ 			status: "pending"
+ 		});
+
 	});
-}
+
+	// new Rx table update
+	myFirebaseRef.on('child_added', function(snapshot) {
+		console.log("RxAdded to Firebase");
+
+		var rx = snapshot.val();
+
+		addRxToTable(rx.patientId, rx.drug, rx.status);
+        console.log("RxAdded to table");
+	});
+
+	function addRxToTable(patient_id, drug, status) {
+		$('.table-rx .table > tbody:last').append('<tr>' + 
+			'<td>' + patient_id + '</td>' + 
+			'<td>' + drug.child('0').name + '</td>' + 
+			'<td>' + drug[0].number + '</td>' + 
+			'<td>' + drug[0].mass + '</td>' + 
+			'<td>' + drug[0].quantity + '</td>' + 
+			'<td>' + status + '</td>' + 
+			'</tr>');
+		console.log("addedRxToTable()");
+	};
+});
 
 
