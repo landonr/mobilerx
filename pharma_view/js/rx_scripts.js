3368b89b5b7921694994c2ec37a8a1bcf320607a
Jason Wist <jason.wist@gmail.com>
2014-09-27 19:50:50 -0600

    Table updating
    On load, the table is generated.
    On upload, the table is deleted and generated.

--- pharma_view/js/rx_scripts.js
+++ pharma_view/js/rx_scripts.js
@@ -1,4 +1,5 @@
 var myFirebaseRef = new Firebase("https://amber-inferno-3172.firebaseio.com/");
+var rx_ref = myFirebaseRef.child('rx');
 
 
 $(document).ready(function(){
@@ -11,42 +12,59 @@ $(document).ready(function(){
  		var drug_mass = $('#drug-mass').val();
  		var drug_quantity = $('#drug-quantity').val();
 
- 		var rx_ref = myFirebaseRef.child('rx');
-
  		rx_ref.push({
  			patientId: patient_id,
- 			drug: [{
+ 			drug: {
  				name: drug_name,
  				number: drug_number,
  				mass: drug_mass,
  				quantity: drug_quantity
- 			}],
+ 			},
  			status: "pending"
  		});
 
 	});
-
-	// new Rx table update
-	myFirebaseRef.on('child_added', function(snapshot) {
-		console.log("RxAdded to Firebase");
-
-		var rx = snapshot.val();
-
-		addRxToTable(rx.patientId, rx.drug, rx.status);
-        console.log("RxAdded to table");
-	});
-
-	function addRxToTable(patient_id, drug, status) {
-		$('.table-rx .table > tbody:last').append('<tr>' + 
-			'<td>' + patient_id + '</td>' + 
-			'<td>' + drug.child('0').name + '</td>' + 
-			'<td>' + drug[0].number + '</td>' + 
-			'<td>' + drug[0].mass + '</td>' + 
-			'<td>' + drug[0].quantity + '</td>' + 
-			'<td>' + status + '</td>' + 
-			'</tr>');
-		console.log("addedRxToTable()");
-	};
+	
 });
 
+// new Rx table update
+rx_ref.on('value', function(snapshot) {
+	console.log("Rx added to Firebase");
 
+	$('.table-rx .table > tr').remove();
+
+	console.log(snapshot.val());
+
+	$.each(snapshot.val(), function (key, val) {
+
+		console.log('.eachcalled');
+
+		var rx = '<tr><td>' + val.patientId + '</td>' +
+			'<td>' + val.drug.name + '</td>' +
+			'<td>' + val.drug.number + '</td>' +
+			'<td>' + val.drug.mass + '</td>' +
+			'<td>' + val.drug.quantity + '</td>' +
+			'<td>' + val.status + '</td></tr>';
+	    
+	    $('.table-rx .table > tbody').append(rx);
+    });
+
+	// $('.table-rx .table > tbody').append();
+
+    console.log("Rx table refreshed");
+});
+
+// function addRxToTable(patient_id, drug, status) {
+// 	console.log("addedRxToTable()");
+// 	console.log(drug);
+// 	console.log(drug.name);
+// 	$('.table-rx .table > tbody:last').append('<tr>' + 
+// 		'<td>' + patient_id + '</td>' + 
+// 		'<td>' + drug.name + '</td>' + 
+// 		'<td>' + drug.number + '</td>' + 
+// 		'<td>' + drug.mass + '</td>' + 
+// 		'<td>' + drug.quantity + '</td>' + 
+// 		'<td>' + status + '</td>' + 
+// 		'</tr>');
+		
+// 	};
